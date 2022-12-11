0556039f1748601c5879b718cc997bda66004118
Titux Metal (MBP) <github@lgdweb.fr>
2022-12-11 10:26:44 +0100

    Updating app.controller.ts file; implements all others crud operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common'
+import { v4 as uuid } from 'uuid'
 
 import { data, ReportType } from './data'
 
@@ -24,17 +25,57 @@ export class AppController {
   }
 
   @Post('reports/:type')
-  createReport() {
-    return 'Report Created'
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string
+  ) {
+    const reportType = type === 'incomes' ? ReportType.INCOME : ReportType.EXPENSE
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      type: reportType,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+    data.report.push(newReport)
+
+    return newReport
   }
 
   @Put('reports/:type/:id')
-  updateReport() {
-    return 'Report Updated'
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string }
+  ) {
+    const reportType = type === 'incomes' ? ReportType.INCOME : ReportType.EXPENSE
+
+    const reportToUpdate = data.report
+      .filter(({ type }) => type === reportType)
+      .find(report => report.id === id)
+
+    if (!reportToUpdate) return
+
+    const reportIndex = data.report.findIndex(report => report.id === reportToUpdate.id)
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body
+    }
+
+    return data.report[reportIndex]
   }
 
+  @HttpCode(204)
   @Delete('reports/:type/:id')
-  deleteReport() {
-    return 'Report Deleted'
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex(report => report.id === id)
+
+    if (reportIndex === -1) return
+
+    data.report.splice(reportIndex, 1)
+
+    return
   }
 }
