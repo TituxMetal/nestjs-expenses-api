63270ed75df2ac79193cc365ff484c4e80fd346a
Titux Metal (MBP) <github@lgdweb.fr>
2022-12-11 08:13:58 +0100

    Updating app.controller.ts file; implements getAllReports method plus other dummy crud operation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+
+import { data, ReportType } from './data'
 
 @Controller()
 export class AppController {
   @Get()
-  getHello() {
-    return 'Hello NestJs'
+  welcome() {
+    return 'Welcome to NestJs'
+  }
+
+  @Get('reports/:type')
+  getAllReports(@Param('type') type: string) {
+    const reportType = type === 'incomes' ? ReportType.INCOME : ReportType.EXPENSE
+
+    return data.report.filter(report => report.type === reportType)
+  }
+
+  @Get('reports/:type/:id')
+  getReportById() {
+    return {}
+  }
+
+  @Post('reports/:type')
+  createReport() {
+    return 'Report Created'
+  }
+
+  @Put('reports/:type/:id')
+  updateReport() {
+    return 'Report Updated'
+  }
+
+  @Delete('reports/:type/:id')
+  deleteReport() {
+    return 'Report Deleted'
   }
 }
